763fe8b02aec78086e16ae0281a9717a95ae30ae
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-02-04 22:15:15 +0100

    Nucleotide Count

---
+++ nucleotide-count/nucleotide-count.ts
@@ -0,0 +1,12 @@
+export default class NucleotideCount {
+    static nucleotideCounts(nuc: string) {
+        const translate = new Map([["A", 0], ["C", 0], ["G", 0], ["T", 0]]);
+        [...nuc].forEach(n => {
+            if (translate.has(n)) {
+                translate.set(n, translate.get(n)! + 1)
+            }
+            else { throw Error("Invalid nucleotide in strand") }
+        })
+        return Object.fromEntries(translate)
+    }
+}
